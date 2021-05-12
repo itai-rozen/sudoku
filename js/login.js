1e609233016197330632fb2d982d48b5e95e8339
itai rozen <itai.rozen@mail.huji.ac.il>
2021-05-12 19:37:56 +0300

    sudoku final

--- js/login.js
+++
@@ -1,9 +0,0 @@
-let userLogin = () => {
-    let elUserName = document.getElementById('username');
-    let userNameValue = elUserName.value;
-    let elPassword = document.getElementById('pwd');
-    let passwordValue = elPassword.value;
-
-    if (userNameValue !== 'x' || passwordValue !== 'x') alert('wrong password or username!')
-    else (window.open("./game.html",'_self'))
-}
